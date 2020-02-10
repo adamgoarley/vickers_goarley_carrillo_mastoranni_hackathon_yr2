d7d83f5222e811b36466657a6a2ee29bdd1cbd09
Adam Goarley <agoarley@icloud.com>
2020-02-10 12:53:04 -0500

    adding some basic webstite setup

--- index.php
+++ index.php
@@ -8,29 +8,89 @@
 </head>
 <body>
     <header>
-
+        <input type="text" placeholder="Search..">
+        <img src="" alt="Avatar">
     </header>
 
     <main>
-        <section>
-            <h2 class="hidden">Landing Image Section</h2>
+        <section id="Featured" >
+           <h2 class="">Featured</h2>  <!-- ---------NEEDS TO BE SLIDING IMAGES--------- -->
+            
             <img src="images/landing.jpg" alt="landing image">
         </section>
 
-        <section>
-            <h2>WHY ONTARIO?</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam reprehenderit similique alias quaerat excepturi autem perspiciatis quae earum. Impedit deserunt delectus sint molestias quod aliquam recusandae maxime est rem ipsa!</p>
+        <section id="Categories">
+            <h2>Categories</h2>
+
+            <div id="category_boxs">
+                        <div class="cat1 cat">
+                            <img class="catImgs" src="./public/images/test.jpg" alt="">
+                            <h3 class="cat1Text">Hotels</h3>
+                        </div>
+    
+                        <div class="cat2 cat">
+                            <img class="catImgs" src="./public/images/test.jpg" alt="">
+                            <h3 class="cat1Text">Entertainment</h3>
+                        </div>
+    
+                        <div class="cat3 cat">
+                            <img class="catImgs" src="./public/images/test.jpg" alt="">
+                            <h3 class="cat1Text">Sightseeing</h3>
+                        </div>
+    
+                        <div class="cat4 cat">
+                            <img class="catImgs" src="./public/images/test.jpg" alt="">
+                            <h3 class="cat1Text">Adventures</h3>
+                        </div>
+
+                        <div class="cat5 cat">
+                            <img class="catImgs" src="./public/images/test.jpg" alt="">
+                            <h3 class="cat1Text">Food</h3>
+                        </div>
+    
+                        <div class="cat6 cat">
+                            <img class="catImgs" src="./public/images/test.jpg" alt="">
+                            <h3 class="cat1Text">Nature</h3>
+                        </div>
+            </div>
+
+            <div id="catButton">
+                <a href="#" class="catButton">View All</a>
+            </div>
+            
         </section>
 
-        <section>
-            <h2>GET OUT AND EXPLORE!</h2>
-            <video src="video/ontario_ad.mp4"></video>
+        <section id="AroundYou">
+            <h2>Around You</h2>
+
+            <div id="aroundYou_boxs ">
+                        <div class="cat1">
+                            <img class="catImgs" src="./public/images/test.jpg" alt="">
+                            <h3 class="">TEXT</h3>
+                        </div>
+    
+                        <div class="cat2">
+                            <img class="catImgs" src="./public/images/test.jpg" alt="">
+                            <h3 class="">TEXT</h3>
+                        </div>
+    
+                        <div class="cat3">
+                            <img class="catImgs" src="./public/images/test.jpg" alt="">
+                            <h3 class="">TEXT</h3>
+                        </div>
+    
+                        <div class="cat4">
+                            <img class="catImgs" src="./public/images/test.jpg" alt="">
+                            <h3 class="">TEXT</h3>
+                        </div>
+            </div>
+
+            <div id="catButton">
+                <p>View All</p>
+            </div>
+            
         </section>
 
-        <section>
-            <h2>Sign up for our mailing list!</h2>
-            <button><a href="admin/signupForm.php">SIGN UP</a></button>
-        </section>
     </main>
 </body>
 </html>
